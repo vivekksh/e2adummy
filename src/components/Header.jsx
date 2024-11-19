9e70b7f2db5e4470b2cea0f7822ca1cb8caa391b
Vinayak Das <vinayakd909@gmail.com>
2024-11-19 22:18:36 +0530

    ADD: Sponsor Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "../assets/css/header.css";
 import DropDown from "./DropDown";
 import ReactDOM from "react-dom";
-import CrossfadeCarousel from '@notbaldrick/react-crossfade-carousel'
+import CrossfadeCarousel from "@notbaldrick/react-crossfade-carousel";
 import image3 from "/images/depImg2.jpeg";
 import image4 from "/images/depImg3.jpeg";
-import image5 from "/images/homeImg_a.jpg"
-import image6 from "/images/homeImg_b.jpg"
-import image7 from "/images/homeImg_c.jpg"
-import image8 from "/images/homeImg_d.jpg"
-import image9 from "/images/homeImg_e.jpg"
-import image10 from "/images/homeImg_f.jpg"
+import image5 from "/images/homeImg_a.jpg";
+import image6 from "/images/homeImg_b.jpg";
+import image7 from "/images/homeImg_c.jpg";
+import image8 from "/images/homeImg_d.jpg";
+import image9 from "/images/homeImg_e.jpg";
+import image10 from "/images/homeImg_f.jpg";
 import newLogo from "/logos/tnew.gif";
 import { Link } from "react-router-dom";
 
@@ -102,25 +102,34 @@ const Header = () => {
   }, []);
 
   const handleClickScrollHome = () => {
-    const element = document.getElementById('about');
+    const element = document.getElementById("about");
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   const handleClickScrollHero = () => {
-    const element = document.getElementById('header');
+    const element = document.getElementById("header");
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
     <>
       <div className="header" id="header">
-        <CrossfadeCarousel interval={2000} transition={2000} 
+        <CrossfadeCarousel
+          interval={2000}
+          transition={2000}
           images={[
-            image8,image7,image9,image10,image6,image4,image5,image3
+            image8,
+            image7,
+            image9,
+            image10,
+            image6,
+            image4,
+            image5,
+            image3,
           ]}
         />
         <nav className={isScrolled ? "moved nav-bar" : "nav-bar"}>
@@ -248,18 +257,21 @@ const Header = () => {
             <div className="logoBox">
               <div>
                 <h6 className="organised-by bold mt-2 mb-1">
-                  Technically Co-Sponsored : 
-                  <span>    </span>
-                   
-              <img src="/logos/logo_springer.jpg" alt="" className="nit-logo" />
-          
+                  Technically Co-Sponsored :<span> </span>
+                  {/* <img
+                    src="/logos/logo_springer.jpg"
+                    alt=""
+                    className="nit-logo"
+                  /> */}
                 </h6>
               </div>
               <div className="lBox">
-                {/* <img className="serbLogo" src="/logos/serb_logo.png" alt="" />
+                {/* <img className="serbLogo" src="/logos/serb_logo.png" alt="" /> */}
+              <img className="logo2" src="/logos/digiToad.jpg" alt="" />
               <img className="logo2" src="/logos/logo_springer.jpg" alt="" />
-              <img className="logo2" src="/logos/infoTechLogo.jpg" alt="" />
-              <img className="logo2" src="/logos/bmgLogo.jpg" alt="" /> */}
+              <img className="logo2" src="/logos/mantisWave.jpg" alt="" />
+              {/* <img className="logo2" src="/logos/infoTechLogo.jpg" alt="" /> */}
+              {/* <img className="logo2" src="/logos/bmgLogo.jpg" alt="" /> */}
               </div>
             </div>
           </div>
